81b2ce8349ea29f76dcb9e2627a1a3762062840a
Ryosuke Yamaguchi <ryosuke_yama@RyosukenoMacBook-Air.local>
2024-02-25 13:20:25 +0900

    edit: README

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ Mint coeff(int q, int m, int n, int k) {
     return ret;
 }
 
-// O(q^{3m})
+// O(q^{2m})
 bool check(int q, int m) {
     int n = 1;
     for (int i = 0; i < m - 1; i++) n *= q;
@@ -27,9 +27,24 @@ bool check(int q, int m) {
     return ret;
 }
 
+bool is_primepower(int q) {
+    // count the number of prime factors
+    int cnt = 0;
+    for (int d = 2; d * d <= q; d++) {
+        if (q % d != 0) continue;
+        cnt++;
+        while (q % d == 0) {
+            q /= d;
+        }
+    }
+    if (q > 1) cnt++;
+    return cnt == 1;
+}
+
 int main(){
-    std::vector<int> qs({3, 4, 5, 7, 8, 9, 11, 13, 16, 17, 19});
-    for (int q: qs) {
+    for (int q = 3; q <= 200; q++) {
+        if (!is_primepower(q)) continue;
+        std::cout << " q: " << q << std::endl;
         for (int m = 2; m <= 20; m++) {
             if (std::pow(q, m) > MAX_N) continue;
             if (std::pow(q, 2 * m) > MAX_ITERATE) continue;
